cb7b3f434140a7565741620701ac12bf1a3a7cc7
AssIstne <463636701@qq.com>
2016-04-28 23:27:35 +0800

    finish static UI

--- app/src/main/java/com/assistne/dribbble/framestday004/CardFragment.java
+++ app/src/main/java/com/assistne/dribbble/framestday004/CardFragment.java
@@ -1,11 +1,17 @@
 package com.assistne.dribbble.framestday004;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.support.annotation.DrawableRes;
 import android.support.annotation.Nullable;
+import android.support.annotation.StringRes;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.assistne.dribbble.R;
 
@@ -13,10 +19,60 @@ import com.assistne.dribbble.R;
  * Created by assistne on 16/4/26.
  */
 public class CardFragment extends Fragment{
+    private static final String TAG = "#CardFragment";
+
+    private static final String KEY_CARD = "CARD";
+    private static final String KEY_STAR = "STAR";
+    private static final String KEY_COMMENT = "COMMENT";
+    private static final String KEY_AVATAR = "AVATAR";
+
+    private View mContainer;
+    private ImageView mCard;
+    private ImageView mStar;
+    private TextView mComment;
+    private ImageView mAvatar;
+
+    public static CardFragment newInstance(@DrawableRes int card, @DrawableRes int star,
+                                           @StringRes int comment, @DrawableRes int avatar) {
+        CardFragment fragment = new CardFragment();
+        Bundle bundle = new Bundle();
+        bundle.putInt(KEY_CARD, card);
+        bundle.putInt(KEY_STAR, star);
+        bundle.putInt(KEY_COMMENT, comment);
+        bundle.putInt(KEY_AVATAR, avatar);
+        fragment.setArguments(bundle);
+        return fragment;
+
+    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_fst_d4_card, container, false);
+        View root = inflater.inflate(R.layout.fragment_fst_d4_card, container, false);
+        mContainer = root.findViewById(R.id.fst_d4_container);
+        Log.d(TAG, "onCreateView: " + this + "  " +  mContainer);
+        mCard = (ImageView) root.findViewById(R.id.fst_d4_card_img);
+        mStar = (ImageView) root.findViewById(R.id.fst_d4_star);
+        mComment = (TextView) root.findViewById(R.id.fst_d4_card_comment);
+        mAvatar = (ImageView) root.findViewById(R.id.fst_d4_card_avatar);
+        Bundle bundle = getArguments();
+        mCard.setImageResource(bundle.getInt(KEY_CARD));
+        mStar.setImageResource(bundle.getInt(KEY_STAR));
+        mComment.setText(bundle.getInt(KEY_COMMENT));
+        mAvatar.setImageResource(bundle.getInt(KEY_AVATAR));
+        return root;
+    }
+
+    public void show() {
+        Log.d(TAG, "show: " + this + "  " +mContainer);
+        if (mContainer != null) {
+            mContainer.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void hide() {
+        if (mContainer != null) {
+            mContainer.setVisibility(View.GONE);
+        }
     }
 }
--- app/src/main/java/com/assistne/dribbble/framestday004/FStD4Activity.java
+++ app/src/main/java/com/assistne/dribbble/framestday004/FStD4Activity.java
@@ -10,6 +10,7 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.assistne.dribbble.R;
 
@@ -29,12 +30,32 @@ public class FStD4Activity extends AppCompatActivity {
         setContentView(R.layout.activity_fst_d4);
         mViewPager = (ViewPager) findViewById(R.id.fst_d4_viewpager);
         mViewPager.setAdapter(new MyPageAdapter(getSupportFragmentManager()));
-        Log.d(TAG, "onCreate: " + mViewPager.getPageMargin());
-        mViewPager.setPageMargin(-400);
+        mViewPager.setOffscreenPageLimit(2);
+        mViewPager.setPageMargin(-250);
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {}
+
+            @Override
+            public void onPageSelected(int position) {
+                Log.d(TAG, "onPageSelected: " + position);
+                FragmentPagerAdapter adapter = (FragmentPagerAdapter) mViewPager.getAdapter();
+                for (int i = 0; i < adapter.getCount(); i ++) {
+                    CardFragment fragment = (CardFragment) adapter.getItem(i);
+                    if (i == position) {
+                        fragment.show();
+                    } else {
+                        fragment.hide();
+                    }
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {}
+        });
     }
 
     private class MyPageAdapter extends FragmentPagerAdapter {
-
         public MyPageAdapter(FragmentManager fm) {
             super(fm);
         }
@@ -46,7 +67,31 @@ public class FStD4Activity extends AppCompatActivity {
 
         @Override
         public Fragment getItem(int position) {
-            return new CardFragment();
+            Log.d(TAG, "getItem: " + position);
+            switch (position) {
+                case 0:
+                    return CardFragment.newInstance(R.drawable.fst_day4_wt, R.drawable.fst_day4_wt_star,
+                            R.string.wt_comment, R.drawable.fst_day4_avatar3);
+                case 1:
+                    return CardFragment.newInstance(R.drawable.fst_day4_ny, R.drawable.fst_day4_ny_star,
+                            R.string.ny_comment, R.drawable.fst_day4_avatar2);
+                case 2:
+                    return CardFragment.newInstance(R.drawable.fst_day4_bb, R.drawable.fst_day4_bb_star,
+                            R.string.bb_comment, R.drawable.fst_day4_avatar1);
+            }
+            return null;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            Log.d(TAG, "instantiateItem: " + position);
+            return super.instantiateItem(container, position);
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            Log.d(TAG, "destroyItem: " + position + "  " + object);
+            super.destroyItem(container, position, object);
         }
     }
 }
