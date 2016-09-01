bdd81d22adccfe873f0799be1eb67aa5efbb6b18
AssIstne <ljiaquan929@gmail.com>
2016-09-01 18:02:44 +0800

    intermediate

--- app/src/main/java/com/assistne/dribbble/bezierindicator/BezierActivity.java
+++ app/src/main/java/com/assistne/dribbble/bezierindicator/BezierActivity.java
@@ -1,25 +1,79 @@
 package com.assistne.dribbble.bezierindicator;
 
 import android.app.Activity;
+import android.graphics.Color;
+import android.media.Image;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 import com.assistne.dribbble.R;
 
 /**
  * Created by assistne on 16/9/1.
  */
-public class BezierActivity extends Activity {
-
+public class BezierActivity extends AppCompatActivity {
+    private static final String TAG = "#BezierActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bezier_indicator);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(final int position) {
+                return new Fragment(){
+                    @Nullable
+                    @Override
+                    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+                        FrameLayout root = new FrameLayout(getActivity());
+                        root.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+                        if (position == 0) {
+                            root.setBackgroundColor(Color.RED);
+                        } else {
+                            root.setBackgroundColor(Color.BLUE);
+                        }
+                        return root;
+                    }
+                };
+            }
 
+            @Override
+            public int getCount() {
+                return 2;
+            }
+        });
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                Log.d(TAG, "onPageScrolled: " + positionOffset + "  " + positionOffsetPixels);
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+        final BezierIndicatorView indicatorView = (BezierIndicatorView) findViewById(R.id.bezier_indicator);
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                indicatorView.moveBy(150);
+//                indicatorView.bounce();
             }
         });
     }
--- app/src/main/java/com/assistne/dribbble/bezierindicator/BezierIndicatorView.java
+++ app/src/main/java/com/assistne/dribbble/bezierindicator/BezierIndicatorView.java
@@ -1,22 +1,140 @@
 package com.assistne.dribbble.bezierindicator;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PointF;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
+import android.view.animation.BounceInterpolator;
 
 /**
+ * 仅水平位移
+ * http://blog.csdn.net/sinat_27706697/article/details/49836627
+ * https://dribbble.com/shots/2227686-Settings-fun-navigation
+ * http://www.jianshu.com/p/791d3a791ec2
  * Created by assistne on 16/9/1.
  */
 public class BezierIndicatorView extends View {
+    private static final String TAG = "#BezierIndicatorView";
+    private Paint mPaint;
+    private Path mPath;
+    private static final float MAGIC_FACTOR = 0.552284749831f;
+    private float mDifference;
+    private float mRadius;
+    private PointF mDataLeft;
+    private PointF mDataTop;
+    private PointF mDataRight;
+    private PointF mDataBottom;
+
     public BezierIndicatorView(Context context) {
-        super(context);
+        this(context, null);
     }
 
     public BezierIndicatorView(Context context, AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs, 0);
     }
 
     public BezierIndicatorView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        mPaint = new Paint();
+        mPaint.setColor(Color.RED);
+        mPaint.setStyle(Paint.Style.FILL);
+        mPaint.setStrokeWidth(8);
+        mPath = new Path();
+
+        mRadius = 100;
+        mDifference = mRadius * MAGIC_FACTOR;
+        mDataLeft = new PointF(0, 0);
+        mDataTop = new PointF(mRadius, -mRadius);
+        mDataRight = new PointF(2 * mRadius, 0);
+        mDataBottom = new PointF(mRadius, mRadius);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.save();
+        canvas.translate(0, getHeight()/2);
+        calculateBezierPath(mPath);
+        canvas.drawPath(mPath, mPaint);
+        canvas.restore();
+    }
+
+    private void getCirclePath(Path path, float centerX, float centerY, float radius) {
+        float circleLeft = centerX - radius;
+        float circleRight = centerX + radius;
+        float circleTop = centerY - radius;
+        float circleBottom = centerY + radius;
+        float difference = radius * MAGIC_FACTOR;
+
+        path.moveTo(circleLeft, centerY);
+        path.cubicTo(circleLeft, centerY - difference, centerX - difference, circleTop, centerX, circleTop);
+        path.cubicTo(centerX + difference, circleTop, circleRight, centerY - difference, circleRight, centerY);
+        path.cubicTo(circleRight, centerY + difference, centerX + difference, circleBottom, centerX, circleBottom);
+        path.cubicTo(centerX - difference, circleBottom, circleLeft, centerY + difference, circleLeft, centerY);
+    }
+
+    private void calculateBezierPath(Path path) {
+        path.reset();
+        path.moveTo(mDataLeft.x, mDataLeft.y);
+        path.cubicTo(mDataLeft.x, mDataLeft.y - mDifference, mDataTop.x - mDifference, mDataTop.y, mDataTop.x, mDataTop.y);
+        path.cubicTo(mDataTop.x + mDifference, mDataTop.y, mDataRight.x, mDataRight.y - mDifference, mDataRight.x, mDataRight.y);
+        path.cubicTo(mDataRight.x, mDataRight.y + mDifference, mDataBottom.x + mDifference, mDataBottom.y, mDataBottom.x, mDataBottom.y);
+        path.cubicTo(mDataBottom.x - mDifference, mDataBottom.y, mDataLeft.x, mDataLeft.y + mDifference, mDataLeft.x, mDataLeft.y);
+    }
+
+    public void moveBy(final float x) {
+        final float originX = mDataRight.x;
+        ValueAnimator animator = ValueAnimator.ofFloat(0, x, 0);
+        animator.setDuration(5000);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float value = (float) animation.getAnimatedValue();
+                mDifference = mRadius * MAGIC_FACTOR * (1 + animation.getAnimatedFraction()/3);
+                    float delta = originX + value - mDataRight.x;
+                    Log.d(TAG, "onAnimationUpdate: " + delta);
+                    mDataRight.x = originX + value;
+                    if (mDataRight.x - mDataTop.x > 1.5 * mRadius) {
+                        mDataTop.x += delta;
+                        mDataBottom.x += delta;
+                    }
+                    if (mDataTop.x - mDataLeft.x > 1.5 * mRadius) {
+                        mDataLeft.x += delta;
+                    }
+                invalidate();
+            }
+        });
+        animator.start();
+    }
+
+    public void bounce() {
+        final float originX = mDataRight.x;
+        ValueAnimator animator = ValueAnimator.ofFloat(0, 100);
+        animator.setInterpolator(new BounceInterpolator());
+        animator.setDuration(5000);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float value = (float) animation.getAnimatedValue();
+                mDifference = mRadius * MAGIC_FACTOR * (1 + animation.getAnimatedFraction()/2);
+                Log.d(TAG, "onAnimationUpdate: " + value);
+                float delta = originX + value - mDataRight.x;
+                mDataRight.x = originX + value;
+                if (mDataRight.x - mDataTop.x > 1.5 * mRadius) {
+                    mDataTop.x += delta;
+                    mDataBottom.x += delta;
+                }
+                if (mDataTop.x - mDataLeft.x > 1.5 * mRadius) {
+                    mDataLeft.x += delta;
+                }
+                invalidate();
+            }
+        });
+        animator.start();
     }
 }
