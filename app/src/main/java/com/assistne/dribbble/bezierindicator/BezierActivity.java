c29dc730373ea78112b2b139e008f19f95e4f948
AssIstne <ljiaquan929@gmail.com>
2016-09-06 17:26:00 +0800

    optimize import

--- app/src/main/java/com/assistne/dribbble/bezierindicator/BezierActivity.java
+++ app/src/main/java/com/assistne/dribbble/bezierindicator/BezierActivity.java
@@ -5,7 +5,6 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
 import com.assistne.dribbble.R;
 
