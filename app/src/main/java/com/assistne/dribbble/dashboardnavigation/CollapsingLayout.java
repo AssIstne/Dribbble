2d5a2adc32eefc23ffc09dfe02b241f6c1aa985f
AssIstne <ljiaquan929@gmail.com>
2017-02-22 17:51:40 +0800

    add a custom layout for container

---
+++ app/src/main/java/com/assistne/dribbble/dashboardnavigation/CollapsingLayout.java
@@ -0,0 +1,144 @@
+package com.assistne.dribbble.dashboardnavigation;
+
+import android.content.Context;
+import android.support.design.widget.AppBarLayout;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.view.WindowInsetsCompat;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewParent;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by assistne on 17/2/22.
+ */
+
+public class CollapsingLayout extends RelativeLayout {
+    private static final String TAG = "#CollapsingLayout";
+    private WindowInsetsCompat mLastInsets;
+    private AppBarLayout.OnOffsetChangedListener mOnOffsetChangedListener;
+    public CollapsingLayout(Context context) {
+        this(context, null);
+    }
+
+    public CollapsingLayout(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public CollapsingLayout(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        ViewCompat.setOnApplyWindowInsetsListener(this,
+                new android.support.v4.view.OnApplyWindowInsetsListener() {
+                    @Override
+                    public WindowInsetsCompat onApplyWindowInsets(View v,
+                                                                  WindowInsetsCompat insets) {
+                        return onWindowInsetChanged(insets);
+                    }
+                });
+    }
+
+    private WindowInsetsCompat onWindowInsetChanged(final WindowInsetsCompat insets) {
+        WindowInsetsCompat newInsets = null;
+
+        if (ViewCompat.getFitsSystemWindows(this)) {
+            // If we're set to fit system windows, keep the insets
+            newInsets = insets;
+        }
+
+        // If our insets have changed, keep them and invalidate the scroll ranges...
+        if (mLastInsets != newInsets && (mLastInsets != null && mLastInsets.equals(newInsets))) {
+            mLastInsets = newInsets;
+            requestLayout();
+        }
+
+        return insets;
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+
+        // Add an OnOffsetChangedListener if possible
+        final ViewParent parent = getParent();
+        if (parent instanceof AppBarLayout) {
+            // Copy over from the ABL whether we should fit system windows
+            ViewCompat.setFitsSystemWindows(this, ViewCompat.getFitsSystemWindows((View) parent));
+
+            if (mOnOffsetChangedListener == null) {
+                mOnOffsetChangedListener = new OffsetUpdateListener();
+            }
+            ((AppBarLayout) parent).addOnOffsetChangedListener(mOnOffsetChangedListener);
+
+            // We're attached, so lets request an inset dispatch
+            ViewCompat.requestApplyInsets(this);
+        }
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        // Remove our OnOffsetChangedListener if possible and it exists
+        final ViewParent parent = getParent();
+        if (mOnOffsetChangedListener != null && parent instanceof AppBarLayout) {
+            ((AppBarLayout) parent).removeOnOffsetChangedListener(mOnOffsetChangedListener);
+        }
+
+        super.onDetachedFromWindow();
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        super.onLayout(changed, l, t, r, b);
+        Log.d(TAG, "onLayout: ");
+        // Update our child view offset helpers
+        for (int i = 0, z = getChildCount(); i < z; i++) {
+            final View child = getChildAt(i);
+
+            if (mLastInsets != null && !ViewCompat.getFitsSystemWindows(child)) {
+                final int insetTop = mLastInsets.getSystemWindowInsetTop();
+                if (child.getTop() < insetTop) {
+                    // If the child isn't set to fit system windows but is drawing within the inset
+                    // offset it down
+                    ViewCompat.offsetTopAndBottom(child, insetTop);
+                }
+            }
+
+            getViewOffsetHelper(child).onViewLayout();
+        }
+    }
+
+    private class OffsetUpdateListener implements AppBarLayout.OnOffsetChangedListener {
+        @Override
+        public void onOffsetChanged(AppBarLayout layout, int verticalOffset) {
+            Log.d(TAG, "onOffsetChanged: " + verticalOffset);
+            for (int i = 1, z = getChildCount(); i < z; i++) {
+                final View child = getChildAt(i);
+                final ViewOffsetHelper offsetHelper = getViewOffsetHelper(child);
+                int offset = Math.max(Math.min(-verticalOffset, 0), getMaxOffsetForPinChild(child));
+                Log.d(TAG, "child: " + offset);
+                offsetHelper.setTopAndBottomOffset(offset);
+//                offsetHelper.setTopAndBottomOffset(
+//                        Math.round(-verticalOffset * 0.5f));
+            }
+        }
+    }
+
+    private static ViewOffsetHelper getViewOffsetHelper(View view) {
+        ViewOffsetHelper offsetHelper = (ViewOffsetHelper) view.getTag(android.support.design.R.id.view_offset_helper);
+        if (offsetHelper == null) {
+            offsetHelper = new ViewOffsetHelper(view);
+            view.setTag(android.support.design.R.id.view_offset_helper, offsetHelper);
+        }
+        return offsetHelper;
+    }
+
+
+    final int getMaxOffsetForPinChild(View child) {
+        final ViewOffsetHelper offsetHelper = getViewOffsetHelper(child);
+        final LayoutParams lp = (LayoutParams) child.getLayoutParams();
+        return getHeight()
+                - offsetHelper.getLayoutTop()
+                - child.getHeight()
+                - lp.bottomMargin;
+    }
+}
