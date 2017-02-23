6b5f622f46a4eaa26a14c65cfdc010eb270eabe8
AssIstne <ljiaquan929@gmail.com>
2017-02-23 17:25:00 +0800

    add to demo

--- app/src/main/java/com/assistne/dribbble/dashboardnavigation/DNavigationActivity.java
+++ app/src/main/java/com/assistne/dribbble/dashboardnavigation/DNavigationActivity.java
@@ -5,11 +5,20 @@ import android.support.v7.app.AppCompatActivity;
 
 import com.assistne.dribbble.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class DNavigationActivity extends AppCompatActivity {
+
+    @BindView(R.id.chart)
+    PieChartView mPieChartView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dnavigation);
+        ButterKnife.bind(this);
+
+        mPieChartView.setData(new float[]{5, 4.5f, 2, 4, 7});
     }
 }
