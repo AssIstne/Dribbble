4835bb745874789b11ca30ab3abd9276c70a988e
AssIstne <ljiaquan929@gmail.com>
2017-02-26 21:30:11 +0800

    change the view pager background when rotating

--- app/src/main/java/com/assistne/dribbble/dashboardnavigation/ContentFragment.java
+++ app/src/main/java/com/assistne/dribbble/dashboardnavigation/ContentFragment.java
@@ -1,7 +1,6 @@
 package com.assistne.dribbble.dashboardnavigation;
 
 import android.os.Bundle;
-import android.support.annotation.ColorRes;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -15,24 +14,9 @@ import com.assistne.dribbble.R;
  */
 
 public class ContentFragment extends Fragment {
-public static ContentFragment newInstance(@ColorRes int color) {
-
-        Bundle args = new Bundle();
-        args.putInt("color", color);
-        ContentFragment fragment = new ContentFragment();
-        fragment.setArguments(args);
-        return fragment;
-    }
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_dn_content, container, false);
-    }
-
-    @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        @ColorRes int color = getArguments().getInt("color");
-        ((ViewGroup)view).getChildAt(0).setBackgroundColor(getResources().getColor(color));
     }
 }
--- app/src/main/java/com/assistne/dribbble/dashboardnavigation/DNavigationActivity.java
+++ app/src/main/java/com/assistne/dribbble/dashboardnavigation/DNavigationActivity.java
@@ -1,5 +1,6 @@
 package com.assistne.dribbble.dashboardnavigation;
 
+import android.animation.ArgbEvaluator;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -26,10 +27,11 @@ public class DNavigationActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         mPieChartView.setData(new float[]{5, 4.5f, 2, 4, 7});
+        mViewPager.setBackgroundColor(getResources().getColor(PieChartView.COLOR_ARR[0]));
         mViewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
             @Override
             public Fragment getItem(int position) {
-                return ContentFragment.newInstance(PieChartView.COLOR_ARR[position]);
+                return new ContentFragment();
             }
 
             @Override
@@ -37,22 +39,15 @@ public class DNavigationActivity extends AppCompatActivity {
                 return 5;
             }
         });
+        final ArgbEvaluator evaluator = new ArgbEvaluator();
         mViewPager.addOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-                Log.d(TAG, "onPageScrolled: " + position + "  " + positionOffset + "  " + positionOffsetPixels);
                 mPieChartView.rotateChart(position + positionOffset);
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-                Log.d(TAG, "onPageSelected: " + position);
-//                mPieChartView.setCurrentIndex(position);
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-                Log.d(TAG, "onPageScrollStateChanged: " + state);
+                if (positionOffset > 0) {
+                    int color = (int) evaluator.evaluate(positionOffset, getResources().getColor(PieChartView.COLOR_ARR[position]), getResources().getColor(PieChartView.COLOR_ARR[position+1]));
+                    mViewPager.setBackgroundColor(color);
+                }
             }
         });
     }
